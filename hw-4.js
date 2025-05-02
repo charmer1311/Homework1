405d9075f1700e43db9d942eef0a8871f1856172
charmer1311 <visharipova@yandex.ru>
2025-05-02 16:13:56 +0300

    Задание по JS сделаны

---
+++ hw-4.js
@@ -0,0 +1,78 @@
+// Задание 1
+let a = 20;
+alert(a);
+
+// Задание 2
+let year = 2007;
+alert(year);
+
+// Задание 3
+let name = 'Brendan Eich';
+alert(name);
+
+// Задание 4
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+// Задание 5
+let result = 2 ** 5; //32
+alert(result);
+
+//Задание 6
+let d = 9;
+let f = 2;
+let remainder = 9 % 2;
+alert(remainder);
+
+//Задание 7
+let num = 1;
+alert(num += 5);
+alert(num -= 3);
+alert(num *= 7);
+alert(num /= 3);
+alert(num++);
+alert(num--);
+alert(num);
+
+//Задание 8
+let age = prompt('Сколько вам лет');
+alert(age);
+
+
+//Задание 9
+const user = {
+    name: "Vika",
+    age: 38,
+    isAdmin: true
+};
+
+//Задание 10
+const firstName = prompt('Как вас зовут?');
+alert(`Привет ${firstName}!`);
+
+
+
+//Дополнительное задание
+let number = Number(prompt('Загадайте любое число'));
+let z = 2;
+let product = number * z;
+alert(product);
+let y = 10
+let sum = product + y;
+alert(sum);
+let v = 2
+let quotient = sum / v;
+alert(quotient);
+let difference = quotient - number;
+alert(difference);
+alert(`Ответ равен ${difference}`)
+
+
+
+
+
+
