00834a349c74a01726d9af93b7d59cbdcb63621c
charmer1311 <visharipova@yandex.ru>
2025-05-16 21:02:17 +0300

    Первая игра сделана

--- hw-5.js
+++ hw-5.js
@@ -1,108 +1,108 @@
-// //Задание 1
-// let password = 'пароль';
-// let userPas = String(prompt('Введите пароль'));
-// if (userPas === password) {
-//     console.log('Пароль введен верно');
-// } else {
-//     console.log("Пароль введен неправильно");
-// }
+//Задание 1
+let password = 'пароль';
+let userPas = String(prompt('Введите пароль'));
+if (userPas === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log("Пароль введен неправильно");
+}
 
-// // Задание 2
-// let c = 0;
-// if (c > 0 && c < 10) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
-// }
+// Задание 2
+let c = 0;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
 
-// // Задание 3
-// let d = 20;
-// let e = 103;
-// if (d > 100 || e > 100) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
-// }
+// Задание 3
+let d = 20;
+let e = 103;
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
 
-// // Задание 4 - доработка домашки
-// let a = '2';
-// let b = '3';
-// console.log(Number(a) + Number(b));
+// Задание 4 - доработка домашки
+let a = '2';
+let b = '3';
+console.log(Number(a) + Number(b));
 
-// //Задание 5
-// let monthNumber = 8;
-// switch (monthNumber) {
-//     case 1:
-//     case 2:
-//     case 12:
-//         console.log('Зима');
-//         break;
-//     case 3:
-//     case 4:
-//     case 5:
-//         console.log('Весна');
-//         break;
-//     case 6:
-//     case 7:
-//     case 8:
-//         console.log('Лето');
-//         break;
-//     case 9:
-//     case 10:
-//     case 11:
-//         console.log('Осень');
-//         break;
-//     default:
-//         console.log('что то не так')
-//         break;
-// }
-// // Дополнительное Задание 1
-// let g = (prompt('Пожалуйста, введите любое число'));
-// let f = Number(g);
-// if (isNaN(f)) {
-//     alert("Не является числом");
+//Задание 5
+let monthNumber = 8;
+switch (monthNumber) {
+    case 1:
+    case 2:
+    case 12:
+        console.log('Зима');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна');
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осень');
+        break;
+    default:
+        console.log('что то не так')
+        break;
+}
+// Дополнительное Задание 1
+let g = (prompt('Пожалуйста, введите любое число'));
+let f = Number(g);
+if (isNaN(f)) {
+    alert("Не является числом");
 
-// } else {
+} else {
 
-//     if (g % 2 == 0) {
-//         alert('четное');
+    if (g % 2 == 0) {
+        alert('четное');
 
-//     } else {
-//         alert('нечетное');
-//     }
-// }
-// //Дополнительное Задание 2 и 3
-// let operatingSystem = prompt('Какая у вас операционная система:0 — iOS, 1 — Android');
-// let clientOS = parseInt(operatingSystem);
+    } else {
+        alert('нечетное');
+    }
+}
+//Дополнительное Задание 2 и 3
+let operatingSystem = prompt('Какая у вас операционная система:0 — iOS, 1 — Android');
+let clientOS = parseInt(operatingSystem);
 
-// let release = prompt('Введите год выпуска устройства');
-// let old = Number(release);
-// let clientDeviceYear = 2015;
+let release = prompt('Введите год выпуска устройства');
+let old = Number(release);
+let clientDeviceYear = 2015;
 
 
-// if (clientOS === 0) {
-//     if (old <= clientDeviceYear) {
-//         alert('Установите облегченную версию приложения для iOS по ссылке');
+if (clientOS === 0) {
+    if (old <= clientDeviceYear) {
+        alert('Установите облегченную версию приложения для iOS по ссылке');
 
-//     } else {
-//         alert('Установите версию приложения для iOS по ссылке');
-//     }
+    } else {
+        alert('Установите версию приложения для iOS по ссылке');
+    }
 
-// } else if (clientOS === 1) {
-//     if (old <= clientDeviceYear) {
-//         alert('Установите облегченную версию приложения для Android по ссылке');
+} else if (clientOS === 1) {
+    if (old <= clientDeviceYear) {
+        alert('Установите облегченную версию приложения для Android по ссылке');
 
-//     } else {
-//         alert('Установите версию приложения для Android по ссылке')
+    } else {
+        alert('Установите версию приложения для Android по ссылке')
 
-//     }
+    }
 
-// } else {
-//     alert('Вввели что-то не то')
-// }
+} else {
+    alert('Вввели что-то не то')
+}
 
-// for (let n = 1000; n < 50;  n /= 2) {
-//     console.log(n);
+for (let n = 1000; n < 50;  n /= 2) {
+    console.log(n);}
 
 
 
