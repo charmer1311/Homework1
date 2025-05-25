fd6cd3d34d2f8dc2ad199fb226926ff9a5231e05
charmer1311 <visharipova@yandex.ru>
2025-05-25 14:09:04 +0300

    Исправлена ошибка в игре

--- game1.js
+++ game1.js
@@ -1,22 +1,22 @@
-// function game1() {
-//     const guessNumber = Math.floor(Math.random() * 100) + 1;
-//     while (true) {
-//         const input = Number(prompt('Попробуй угадать число от 1 до 100'));
-//         if (input === 0) {
-//             alert('Попробуй еще!');
-//             return;
-//         }
-//         if (input === guessNumber) {
-//             alert(`Верно! ${guessNumber}`);
-//             break;
-//         }
-//         else if (input < guessNumber) {
-//             alert('Число должно быть больше!');
-//         } else {
-//             alert('Число должно быть меньше')
-//         }
-//     }
-// }
+function game1() {
+    const guessNumber = Math.floor(Math.random() * 100) + 1;
+    while (true) {
+        const input = Number(prompt('Попробуй угадать число от 1 до 100'));
+        if (input === 0) {
+            alert('Попробуй еще!');
+            return;
+        }
+        if (input === guessNumber) {
+            alert(`Верно! ${guessNumber}`);
+            break;
+        }
+        else if (input < guessNumber) {
+            alert('Число должно быть больше!');
+        } else {
+            alert('Число должно быть меньше')
+        }
+    }
+}
 
 function game2() {
     const operators = ['+', '-', '*', '/'];
@@ -45,7 +45,7 @@ function game2() {
             break;
 
     }
-    if (Number(userAnswer === result)) {
+    if (Number(userAnswer == result)) {
         alert("Верно");
     } else {
         alert("Неверно");
