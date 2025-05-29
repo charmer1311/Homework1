bd1cbc1e91469100ec7a1321bbe218a565955b3e
charmer1311 <visharipova@yandex.ru>
2025-05-29 19:27:15 +0300

    Добавлены игры 3 и 4, сделана домашка hw-9

--- game1.js
+++ game1.js
@@ -1,3 +1,4 @@
+// Игра - 1  Угодай число
 function game1() {
     const guessNumber = Math.floor(Math.random() * 100) + 1;
     while (true) {
@@ -18,6 +19,7 @@ function game1() {
     }
 }
 
+// Игра - 2 Простая арифметика
 function game2() {
     const operators = ['+', '-', '*', '/'];
     const operator = operators[Math.floor(Math.random() * operators.length)];
@@ -51,6 +53,52 @@ function game2() {
         alert("Неверно");
     }
 }
+
+// Игра - 3 Переверни текст
+
+function game3() {
+    let text = prompt('Введите текст');
+    text = text.split('');
+    text = text.reverse();
+    text = text.join('');
+    alert(text);
+}
+
+// Игра - 4 Викторина
+function game4() {
+    const quiz = [
+        {
+            question: "Какой цвет небо?",
+            options: ["\n1. Красный", "\n2. Синий", "\n3. Зеленый"],
+            correctAnswer: 2 // номер правильного ответа
+        },
+        {
+            question: "Сколько дней в неделе?",
+            options: ["\n1. Шесть", "\n2. Семь", "\n3. Восемь"],
+            correctAnswer: 2
+        },
+        {
+            question: "Сколько у человека пальцев на одной руке?",
+            options: ["\n1. Четыре", "\n2. Пять", "\n3. Шесть"],
+            correctAnswer: 2
+        }
+    ];
+    let score = 0;
+    for (let i = 0; i < quiz.length; i++) {
+        let quizQuest = Number(prompt(`Ввидите правильный ответ ${quiz[i].question} ${quiz[i].options}`));
+        if (quizQuest == quiz[i][`correctAnswer`]) {
+            score++;
+            alert('Верно');
+        } else {
+            alert('Неверно');
+        }
+
+
+    }
+    alert(`Вы ответили верно на ${score} вопроса`);
+
+}
+
 
 
 
--- hw-8.js
+++ hw-8.js
@@ -1,62 +1,62 @@
-// Задание 1
-let number = [1, 5, 4, 10, 0, 3];
-i = 0;
-while (i < number.length) {
-    if (number[i] === 10) {
-        console.log('Найдено значение 10');
-        break;
-    }
-    i++;
-}
-// Задание 2
-let numbers = [1, 5, 4, 10, 0, 3];
-for (a = 0; a < numbers.length; a++) {
-    if (numbers[a] === 4) {
-        console.log(a);
-        break;
-    }
-}
+// // Задание 1
+// let number = [1, 5, 4, 10, 0, 3];
+// i = 0;
+// while (i < number.length) {
+//     if (number[i] === 10) {
+//         console.log('Найдено значение 10');
+//         break;
+//     }
+//     i++;
+// }
+// // Задание 2
+// let numbers = [1, 5, 4, 10, 0, 3];
+// for (a = 0; a < numbers.length; a++) {
+//     if (numbers[a] === 4) {
+//         console.log(a);
+//         break;
+//     }
+// }
 
-// Задание 3
-let array = [1, 3, 5, 10, 20];
-array = array.join(' ');
-console.log(array);
-
-
-// Задание 4
-let arr = [
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1]
-];
-for (let arrIn of arr) {
-    for (let element of arrIn) {
-        console.log(element)
-    }
-
-}
+// // Задание 3
+// let array = [1, 3, 5, 10, 20];
+// array = array.join(' ');
+// console.log(array);
 
 
-// Задание 5
-let two = [1, 1, 1];
-two.push(2, 2, 2);
-console.log(two);
+// // Задание 4
+// let arr = [
+//     [1, 1, 1],
+//     [1, 1, 1],
+//     [1, 1, 1]
+// ];
+// for (let arrIn of arr) {
+//     for (let element of arrIn) {
+//         console.log(element)
+//     }
 
-// Задание 6
-let arrSort = [9, 8, 7, 'a', 6, 5];
-arrSort.sort();
-const filteredLetter = arrSort.filter(letter => letter > 0);
-console.log(filteredLetter);
+// }
 
-// Задание 7
-const guessNumber = [9, 8, 7, 6, 5];
-const question = +prompt('Угадай число');
 
-if (guessNumber.includes(question)) {
-    alert('Угадал');
-} else {
-    alert('Не угадал');
-}
+// // Задание 5
+// let two = [1, 1, 1];
+// two.push(2, 2, 2);
+// console.log(two);
+
+// // Задание 6
+// let arrSort = [9, 8, 7, 'a', 6, 5];
+// arrSort.sort();
+// const filteredLetter = arrSort.filter(letter => letter > 0);
+// console.log(filteredLetter);
+
+// // Задание 7
+// const guessNumber = [9, 8, 7, 6, 5];
+// const question = +prompt('Угадай число');
+
+// if (guessNumber.includes(question)) {
+//     alert('Угадал');
+// } else {
+//     alert('Не угадал');
+// }
 
 // Задание 8
 let letters = 'abcdef';
@@ -66,74 +66,74 @@ letters.reverse();
 letters = letters.join('');
 console.log(letters);
 
-// Задание 9
-const numberArr = [[1, 2, 3], [4, 5, 6]];
-const newArray = [];
-for (let i = 0; i < numberArr.length; i++) {
-    newArray.push(...numberArr[i]);
-}
-console.log(newArray);
+// // Задание 9
+// const numberArr = [[1, 2, 3], [4, 5, 6]];
+// const newArray = [];
+// for (let i = 0; i < numberArr.length; i++) {
+//     newArray.push(...numberArr[i]);
+// }
+// console.log(newArray);
 
-// Задание 10
-const arb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let g = 0; g < arb.length; g++) {
-    if (arb[g + 1]) {
-        console.log(arb[g] + arb[g + 1]);
+// // Задание 10
+// const arb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let g = 0; g < arb.length; g++) {
+//     if (arb[g + 1]) {
+//         console.log(arb[g] + arb[g + 1]);
 
-    }
-}
+//     }
+// }
 
-// Задание 11
-function square(n) {
-    return n.map(item => item ** 2);
-}
-console.log(square([5, 6, 7,]));
+// // Задание 11
+// function square(n) {
+//     return n.map(item => item ** 2);
+// }
+// console.log(square([5, 6, 7,]));
 
-// Задание 12
-function getLength(j) {
-    return j.map(item1 => item1.length);
-}
-console.log(getLength(['hi', 'apples']));
+// // Задание 12
+// function getLength(j) {
+//     return j.map(item1 => item1.length);
+// }
+// console.log(getLength(['hi', 'apples']));
 
-// Задание 13
-function negativ(arr1) {
-    return arr1.filter(item2 => item2 < 0);
-}
-console.log(negativ([1, 2, -6, 8, -9]))
+// // Задание 13
+// function negativ(arr1) {
+//     return arr1.filter(item2 => item2 < 0);
+// }
+// console.log(negativ([1, 2, -6, 8, -9]))
 
-// Задание 14
-function randomNumber() {
-    return Math.floor(Math.random() * 10);
-}
+// // Задание 14
+// function randomNumber() {
+//     return Math.floor(Math.random() * 10);
+// }
 
-const empty = [];
-for (let l = 0; l < 10; l++) {
-    empty.push(randomNumber());
+// const empty = [];
+// for (let l = 0; l < 10; l++) {
+//     empty.push(randomNumber());
 
-}
-console.log(empty);
-const even = [];
-for (let l = 0; l < empty.length; l++) {
-    if (empty[l] % 2 == 0) {
-        even.push(empty[l]);
+// }
+// console.log(empty);
+// const even = [];
+// for (let l = 0; l < empty.length; l++) {
+//     if (empty[l] % 2 == 0) {
+//         even.push(empty[l]);
 
-    }
+//     }
 
-}
-console.log(even);
+// }
+// console.log(even);
 
-// Задание 15
-function randomNumber1() {
-    return Math.floor(Math.random() * 10);
-}
+// // Задание 15
+// function randomNumber1() {
+//     return Math.floor(Math.random() * 10);
+// }
 
-const empty1 = [];
-for (let k = 0; k < 10; k++) {
-    empty1.push(randomNumber1());
+// const empty1 = [];
+// for (let k = 0; k < 10; k++) {
+//     empty1.push(randomNumber1());
 
-}
-console.log(empty1);
-console.log(empty1.reduce((a, b) => a + b) / empty1.length);
+// }
+// console.log(empty1);
+// console.log(empty1.reduce((a, b) => a + b) / empty1.length);
 
 
 
